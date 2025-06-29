1b22b1cc51984f94488044270c1e83f6d2db10e8
GodishalaAshwith <ashwith7777@gmail.com>
2025-06-29 20:28:28 +0530

    Day 26 CodeChef

---
+++ CodeChef/1000-1400/1053.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input()
+    b = input()
+    if a.count('1')==b.count('1'):
+        print("YES")
+    else:
+        print("NO")
+        

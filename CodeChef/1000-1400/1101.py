e69dea909ea2f8447d59c43b4090e489ba9199a0
GodishalaAshwith <ashwith7777@gmail.com>
2025-07-17 21:44:21 +0530

    Day 44 Codechef

---
+++ CodeChef/1000-1400/1101.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range( int(input())):
+    n,x,c = map(int, input().split())
+    a = list(map(int, input().split()))
+    counter = 0
+    for i in range(n):
+        if a[i]<x and (x-a[i] >c):
+            a[i]=x
+            counter+=1
+    print(sum(a)-(counter*c))

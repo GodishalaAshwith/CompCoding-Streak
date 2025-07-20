eb739c0465dfaeae4d0f4ed491bd883b4f68d2ef
GodishalaAshwith <ashwith7777@gmail.com>
2025-07-20 23:04:34 +0530

    Day 46 FoxNSnake update

--- Codeforces/800/FoxnSnake.py
+++ Codeforces/800/FoxnSnake.py
@@ -1,16 +1,34 @@
-n,m = map(int,input().split())
+# Codeforces submission format
 
-res = [['.' for _ in range(m)] for _ in range(n)]
+def draw_snake():
+    import sys
+    n, m = map(int, sys.stdin.readline().split())
+    
+    for i in range(1, n + 1):
+        if i % 2 == 1:  # Odd rows are fully filled with '#'
+            sys.stdout.write('#' * m + '\n')
+        elif i % 4 == 0:  # Rows like 4th, 8th, etc., have '#' at the beginning
+            sys.stdout.write('#' + '.' * (m - 1) + '\n')
+        else:  # Rows like 2nd, 6th, etc., have '#' at the end
+            sys.stdout.write('.' * (m - 1) + '#\n')
 
-for i in range(0,n,2):
-    res[i][:] = ["#"]*m
+# Call the function directly for Codeforces submission
+draw_snake()
 
-for i in range(1,n,4):
-    res[i][m-1]="#"
 
-if n>3:
-    for i in range(3,n,4):
-        res[i][0]="#"
+# n,m = map(int,input().split())
 
-for i in res:
-    print(*i)
+# res = [['.' for _ in range(m)] for _ in range(n)]
+
+# for i in range(0,n,2):
+#     res[i][:] = ["#"]*m
+
+# for i in range(1,n,4):
+#     res[i][m-1]="#"
+
+# if n>3:
+#     for i in range(3,n,4):
+#         res[i][0]="#"
+
+# for i in res:
+#     print(*i)

1ca1d4a00c2248011b0de7fcd18f4bb80d41b063
GodishalaAshwith <ashwith7777@gmail.com>
2025-07-26 12:06:00 +0530

    Day 53 LC

---
+++ LeetCode/Medium/347.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+            
+        freq_map = Counter(nums)
+        min_heap = []
+        for num, freq in freq_map.items():
+            heapq.heappush(min_heap, (freq, num))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        result = [num for freq, num in min_heap]
+        
+        return result

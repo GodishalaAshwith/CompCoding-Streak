86918d69747f2d4f7b751dbaae428aeab8aa3141
GodishalaAshwith <ashwith7777@gmail.com>
2025-07-26 12:07:12 +0530

    Day 53 LC2

---
+++ LeetCode/Medium/238.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        left, right = 1, 1
+        for i in range(n):
+            res[i] *= left
+            left *= nums[i]
+            res[n - 1 - i] *= right
+            right *= nums[n - 1 - i]
+        return res

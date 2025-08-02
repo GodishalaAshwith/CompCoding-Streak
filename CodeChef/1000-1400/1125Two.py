3418fff28cb5186ceabbe6cf19d7319b81641001
GodishalaAshwith <ashwith7777@gmail.com>
2025-08-02 23:34:29 +0530

    Day 61 Codechef

---
+++ CodeChef/1000-1400/1125Two.py
@@ -0,0 +1,25 @@
+# cook your dish here
+t = int(input())
+
+while t > 0:
+    n = int(input())
+    c = int(0)
+    m = int(0)
+    for i in range(n):
+        a,b = map(str,input().split())
+        a  = sum(map(int,a))
+        b  = sum(map(int,b))
+        if a>b:
+            c+=1
+        elif a<b:
+            m+=1
+        else:
+            c+=1
+            m+=1
+    if c>m:
+        print('0',c)
+    elif c<m:
+        print('1',m)
+    else:
+        print('2',c)
+    t-=1

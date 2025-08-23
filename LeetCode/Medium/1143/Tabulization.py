7ae31c74ca6c252b08c2e0da18a6c9a99d95e633
GodishalaAshwith <ashwith7777@gmail.com>
2025-08-23 22:58:23 +0530

    Day 81 LC2

---
+++ LeetCode/Medium/1143/Tabulization.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n,m = len(text1),len(text2)
+        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = 0
+        for j in range(m+1):
+            dp[0][j] = 0
+        
+        for i1 in range(1,n+1):
+            for i2 in range(1,m+1):
+                if text1[i1-1]==text2[i2-1]: dp[i1][i2] = 1 + dp[i1-1][i2-1]
+                else: dp[i1][i2] = max(dp[i1-1][i2],dp[i1][i2-1])
+        return dp[n][m]

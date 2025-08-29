88883fef6e03a1e65745827aaecc9bf63440472c
GodishalaAshwith <ashwith7777@gmail.com>
2025-08-29 22:43:06 +0530

    Day 87 LC

---
+++ LeetCode/Medium/2149.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        p = 0
+        n = 1
+        res = [None]*len(nums)
+
+        for i in nums:
+            if i > 0:
+                res[p] = i
+                p+=2
+            else:
+                res[n] = i
+                n+=2
+        return res

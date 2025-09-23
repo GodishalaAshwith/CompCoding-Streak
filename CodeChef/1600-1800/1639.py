9c15ebe538d82adf579d0f02031f0c415088acc0
GodishalaAshwith <ashwith7777@gmail.com>
2025-09-23 22:40:26 +0530

    Day 112 Codechef2

---
+++ CodeChef/1600-1800/1639.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    # n=int(input())
+    n, k = map(int, input().split())
+    ans = set()
+    for _ in range(n):
+        x = int(input(), 2)
+        if (x and (not (x & (x - 1)))):
+            ans.add(x)
+    if len(ans) == k:
+        print('Yes')
+    else:
+        print('No')
+
+
+
